4d06d84ee71e9d2a5016447afff5f312b4b2177e
shoenot <mail@shurjo.me>
2025-03-22 05:08:52 +0600

    tag

--- src/tourneyket/__main__.py
+++ src/tourneyket/__main__.py
@@ -14,7 +14,7 @@ from collections.abc import Callable
 __author__ = "Shurjo Sakib Masood"
 __copyright__ = "Copyright 2025, Shurjo Sakib Masood"
 __license__ = "MIT"
-__version__ = "0.1.6"
+__version__ = "1.0.0"
 __email__ = "mail@shurjo.me"
 __status__ = "Production"
 
